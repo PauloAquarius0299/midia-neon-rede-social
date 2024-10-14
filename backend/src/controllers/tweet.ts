c5459627044bae6cb151daedada53662d30ab574
Paulo Cesar <paulocgsantos99@gmail.com>
2024-10-14 12:35:34 -0300

    feat: add tweets like unlike and answers

--- backend/src/controllers/tweet.ts
+++ backend/src/controllers/tweet.ts
@@ -1,7 +1,7 @@
 import { Response } from "express";
 import { ExtendedRequest } from "../types/extended-request";
 import { addTweetSchema } from "../schemas/add-tweet";
-import { createTweet, findTweet } from "../services/tweet";
+import { checkIfTweetIsLikedByUser, createTweet, findAnswersFromTweet, findTweet, likeTweet, unlikeTweet } from "../services/tweet";
 import { addHashtag } from "../services/trend";
 
 export const addTweet = async (req: ExtendedRequest, res: Response) => {
@@ -34,4 +34,42 @@ export const addTweet = async (req: ExtendedRequest, res: Response) => {
     }
 
     res.json({tweet: newTweet});
+};
+
+export const getTweet = async (req: ExtendedRequest, res: Response) => {
+    const {id} = req.params;
+
+    const tweet = await findTweet(parseInt(id));
+    if(!tweet) return res.json({error: 'Tweet inexistente'});
+
+    res.json({ tweet })
+};
+
+export const getAnswers = async (req: ExtendedRequest, res: Response) => {
+    const {id} = req.params;
+
+    const answers = await findAnswersFromTweet(parseInt(id));
+
+    res.json({ answers });
+};
+
+export const likeToggle = async (req: ExtendedRequest, res: Response) => {
+    const {id} = req.params;
+
+    const liked = await checkIfTweetIsLikedByUser(
+        req.userSlug as string,
+        parseInt(id)
+    );
+
+    if(liked){
+        unlikeTweet(
+        req.userSlug as string,
+        parseInt(id)
+        )
+    } else {
+        likeTweet(
+            req.userSlug as string,
+            parseInt(id)
+            )
+    }
 }
